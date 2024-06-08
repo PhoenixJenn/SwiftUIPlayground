eca4ddd5b7719ec59b18a322d344fcd44cdff4ad
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-08 10:52:57 -0700

    Private members in a struct

--- MyPlayground.playground/Pages/Structs.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Structs.xcplaygroundpage/Contents.swift
@@ -172,3 +172,44 @@ struct Player {
 
 let player = Player(name: "Megan R")
 print(player.number)
+
+
+
+// private members
+/*:
+ Use private for “don’t let anything outside the struct use this.”
+ Use fileprivate for “don’t let anything outside the current file use this.”
+ Use public for “let anyone, anywhere use this.”
+ private(set) --- lets anything read but only private modify
+ */
+
+
+struct BankAccount {
+    private var funds = 0
+
+    mutating func deposit(amount: Int) {
+        funds += amount
+    }
+
+    mutating func withdraw(amount: Int) -> Bool {
+        if funds >= amount {
+            funds -= amount
+            return true
+        } else {
+            return false
+        }
+    }
+}
+
+
+
+var account = BankAccount()
+account.deposit(amount: 100)
+let success = account.withdraw(amount: 200)
+
+if success {
+    print("Withdrew money successfully")
+} else {
+    print("Failed to get the money")
+}
+
