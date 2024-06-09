70f9b63ebcbd3a5b62a43b6bfddf8b0e128a7d6f
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-09 08:37:49 -0700

    Completing Protocols, Extensions and Protocol Extensions

--- MyPlayground.playground/Pages/Protocols and Extensions.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Protocols and Extensions.xcplaygroundpage/Contents.swift
@@ -1,8 +1,15 @@
 /*:  [Previous](@previous)    [HOME](Introduction)          [Next](@next)
  
 # Protocols and Extensions
+ Protocols let us define contracts that conforming types must adhere to, and extensions let us add functionality to existing types.
  
+ protocol extensions:
  protocols can’t provide implementations for methods
+ 
+ * Protocols are like contracts for code: we specify the functions and methods that we required, and conforming types must implement them.
+ * Opaque return (ie., using vehicles: [Vehicle]) types let us hide some information in our code. That might mean we want to retain flexibility to change in the future, but also means we don’t need to write out gigantic return types.
+ * Extensions let us add functionality to our own custom types, or to Swift’s built-in types. This might mean adding a method, but we can also add computed properties.
+ * Protocol extensions let us add functionality to many types all at once – we can add properties and methods to a protocol, and all conforming types get access to them.
  
 */
 import Foundation
@@ -63,8 +70,7 @@ struct Bicycle: Vehicle {
 let bike = Bicycle()
 commute(distance: 50, using: bike)
 
-
-
+// ---------- Opaque Return Types - vehicles: [Vehicle]
 func getTravelEstimates(using vehicles: [Vehicle], distance: Int) {
     for vehicle in vehicles {
         let estimate = vehicle.estimateTime(for: distance)
@@ -85,12 +91,13 @@ func getRandomNumber() -> Int {
     Int.random(in: 1...6)
 }
 
+print("here ----------")
+
 print(getRandomNumber() == getRandomNumber())
 
 func getRandomBool() -> Bool {
     Bool.random()
 }
-
 //
 //func getRandomNumber2() -> Equatable {
 //    Int.random(in: 1...6)
@@ -122,3 +129,206 @@ func getRandomBoolX() -> some Equatable {
  
  So, when you see some View in your SwiftUI code, it’s effectively us telling Swift “this is going to send back some kind of view to lay out, but I don’t want to write out the exact thing – you figure it out for yourself.”
  */
+
+
+
+// # Extensions
+
+
+var quote = "   The truth is rarely pure and never simple   "
+
+let trimmed = quote.trimmingCharacters(in: .whitespacesAndNewlines)
+
+
+extension String {
+    func trimmed() -> String {
+        self.trimmingCharacters(in: .whitespacesAndNewlines)
+    }
+}
+
+let trimmed3 = quote.trimmed()
+
+// We could have done this
+
+func trim(_ string: String) -> String {
+    string.trimmingCharacters(in: .whitespacesAndNewlines)
+}
+let trimmed2 = trim(quote)
+
+
+/*:
+ However, the extension has a number of benefits over the global function, including:
+
+ When you type quote. Xcode brings up a list of methods on the string, including all the ones we add in extensions. This makes our extra functionality easy to find.
+ 
+ Writing global functions makes your code rather messy – they are hard to organize and hard to keep track of. On the other hand, extensions are naturally grouped by the data type they are extending.
+ 
+ Because your extension methods are a full part of the original type, they get full access to the type’s internal data. That means they can use properties and methods marked with private access control, for example.
+ */
+
+
+/*:
+ 
+ one property I like to add to strings is called lines, which breaks the string up into an array of individual lines.
+ 
+ This wraps another string method called components(separatedBy:), which breaks the string up into a string array by splitting it on a boundary of our choosing.
+ 
+ In this case we’d want that boundary to be new lines, so we’d add this to our string extension:
+ */
+
+
+
+struct Book {
+    let title: String
+    let pageCount: Int
+    let readingHours: Int
+
+//    init(title: String, pageCount: Int) {
+//        self.title = title
+//        self.pageCount = pageCount
+//        self.readingHours = pageCount / 50
+//    }
+}
+
+
+extension Book {
+    init(title: String, pageCount: Int) {
+        self.title = title
+        self.pageCount = pageCount
+        self.readingHours = pageCount / 50
+    }
+}
+
+let lotr = Book(title: "Lord of the rings", pageCount:356)
+
+
+print (lotr.readingHours)
+
+// an extension on string
+extension String {
+    mutating func append(_ other: String) {
+        self += other
+    }
+}
+
+
+extension Int {
+    func times(_ action: () -> Void) {
+        for _ in 0..<self {
+            action()
+        }
+    }
+}
+
+
+extension Array {
+    func summarize() {
+        print("The array has \(count) items. They are:")
+        for item in self {
+            print(item)
+        }
+    }
+}
+
+
+extension String {
+    func withPrefix(_ prefix: String) -> String {
+        if self.hasPrefix(prefix) { return self }
+        return "\(prefix)\(self)"
+    }
+}
+
+/*:
+--------------------------------------------
+# PROTOCOL EXTENSIONS
+ 
+ */
+print("-------- Protocol Extensions ------------")
+let guests = ["Mario", "Luigi", "Peach"]
+
+if guests.isEmpty == false {
+    print("Guest count: \(guests.count)")
+}
+
+
+if !guests.isEmpty {
+    print("Guest count: \(guests.count)")
+}
+print("--------  Extending Array ------------")
+extension Array {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+if guests.isNotEmpty {
+    print("Guest count: \(guests.count)")
+}
+
+/*:
+ 
+what about sets and dictionaries?\
+ Array, Set, and Dictionary all conform to a built-in protocol called Collection, through which they get functionality such as contains(), sorted(), reversed(), and more.
+ */
+
+extension Collection {
+    var isNotEmpty: Bool {
+        isEmpty == false
+    }
+}
+
+print("--------  Print Set ------------")
+var mySet = Set(["Azrael", "Lucyfur", "Mara Jade"])
+print("--------  Print Set 1 ------------")
+if(mySet.isNotEmpty){ print(mySet)}
+print("--------  Print Set 2 ------------")
+if(mySet.isNotEmpty){mySet.forEach{animal in print(animal)}}
+
+
+
+protocol Person {
+    var name: String { get }
+    func sayHello() ->()
+}
+
+extension Person {
+    func sayHello() {
+        print("Hi, I'm \(name)")
+    }
+}
+
+struct Employee: Person {
+    let name: String
+}
+
+let taylor = Employee(name: "Taylor Swift")
+taylor.sayHello()
+
+
+
+let numbers = [4, 8, 15, 16]
+let allEven = numbers.allSatisfy { $0.isMultiple(of: 2) }
+let numbers2 = Set([4, 8, 15, 16])
+let allEven2 = numbers2.allSatisfy { $0.isMultiple(of: 2) }
+let numbers3 = ["four": 4, "eight": 8, "fifteen": 15, "sixteen": 16]
+let allEven3 = numbers3.allSatisfy { $0.value.isMultiple(of: 2) }
+
+
+print("--------------------")
+
+protocol Anime {
+    var availableLanguages: [String] { get set }
+    func watch(in language: String)
+}
+extension Anime {
+    func watch(in language: String) {
+        if availableLanguages.contains(language) {
+            print("Now playing in \(language)")
+        } else {
+            print("Unrecognized language.")
+        }
+    }
+}
+
+print("--------------------")
+print("This is the end")
+
