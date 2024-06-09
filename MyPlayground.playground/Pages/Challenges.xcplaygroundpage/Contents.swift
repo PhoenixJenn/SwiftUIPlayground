970a268dc7d8bc148a6fdcf19b65a351e926a681
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-09 08:48:03 -0700

    Protocols and Extensions Challenge

--- MyPlayground.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/Challenges.xcplaygroundpage/Contents.swift
@@ -2,6 +2,7 @@
  
  (1) Closures \
  (2) Classes/Structs
+ (3) Protocols and Extensions
  
  
 */
@@ -191,4 +192,51 @@ azriel.isTame = false
 var marajade = Poodle(legs:4, color: "orange")
 marajade.speak()
 
+
+/*:
+ --------------------------------
+ # Protocols and Extensions Challenge
+ 
+ 
+ --make a protocol that describes a building, adding various properties and methods.\
+-- then create two structs, House and Office, that conform to it.
+ 
+ Your protocol should require the following:
+
+*  A property storing how many rooms it has.
+ *  A property storing the cost as an integer (e.g. 500,000 for a building costing $500,000.)
+ *  A property storing the name of the estate agent responsible for selling the building.
+ *  A method for printing the sales summary of the building, describing what it is along with its other properties.
+ 
+ */
+
+protocol Building {
+    var doors: Int {get set}
+    var floors: Int {get set}
+    var rooms: Int {get set}
+    var cost: Int {get set}
+    var agent: String {get set}
+    func printSalesSummary()
+}
+
+struct House : Building {
+    var doors: Int
+    var floors: Int
+    var rooms: Int
+    var cost: Int
+    var agent: String
+    init(doors: Int, floors: Int, rooms: Int, cost: Int, agent: String) {
+        self.doors = doors
+        self.floors = floors
+        self.rooms = rooms
+        self.cost = cost
+        self.agent = agent
+    }
+    func printSalesSummary() {
+        print("This is the Sales Summary")
+    }
+}
+
+let myHouse = House(doors: 3, floors: 3, rooms: 13, cost: 1_025_000, agent: "Ken")
+myHouse.printSalesSummary()
 
