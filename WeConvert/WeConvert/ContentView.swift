d59f805df858a6796929dffb53f5b4a30534fbbd
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-11 16:06:07 -0700

    WeConvert: completed distance conversion calculator

--- WeConvert/WeConvert/ContentView.swift
+++ WeConvert/WeConvert/ContentView.swift
@@ -16,16 +16,7 @@ conversionType: [Temp][Length][Time][Volume]
  --- for consistency, start with largest -> smallest
  
  Temp: Celsius, Fahrenheit
-Distance: meters, kilometers <--> feet, yards, miles
- 1 Miles = Meters / 1609.34708789
- 100 centimeters or 1,000 millimeters
- conversion factor for meters to miles is 0.00062137119. This means that one meter is equal to approximately 0.00062137119 miles. To convert meters to miles, you simply multiply the number of meters by this conversion factor.
- A mile is equivalent to 1760yds or 5280ft.
- 12" per foot
- KM:Miles
- Meters: Feet
- CM:Inches
- MM:Inches
+
  
  Parsecs: Miles
  1pc = 19173501441011 mi
@@ -42,6 +33,7 @@ LiquidVolume: milliliters, liters <--> cups, pints, or gallons.
  
  US gal lqd =L * 0.26417
  
+  the easiest way to store your conversion units is to have a simple string array, which you can loop over using something like ForEach(units, id: \.self).
  
  If you were going for length conversion you might have:
 - A segmented control for meters, kilometers, feet, yard, or miles, for the input unit.
@@ -50,24 +42,173 @@ LiquidVolume: milliliters, liters <--> cups, pints, or gallons.
  - A text view showing the result of the conversion.
  
  So, if you chose meters for source unit and feet for output unit, then entered 10, you’d see 32.81 as the output.
+ 
+ Advanced Picker project
+ https://smehta.medium.com/ios-swift-creating-a-dynamic-picker-view-843b3290e7f0
+ 
  */
 import SwiftUI
 
 struct ContentView: View {
     @State private var conversionType = "distance"
+    @State private var convertFrom = ""  // dynamic assignment of starting units
+    @State private var convertTo = "" // dynamic assignment of ending units
     @State private var amount : Double = 0.0
+   // @State private var myMessage : String = ""
+   
    
     let conversionTypes = ["distance", "time", "temp", "volume"]
-    let distance  = ["miles","yards","feet","inches","kilometers","meters","centimeters","millimeters"]
+    let distance : [String]  = ["","miles","yards","feet","inches","kilometers","meters","centimeters","millimeters"]
     let time = ["days","hours","minutes","seconds"]
     let temp = ["Fahrenheit","Celsius"]
     let volume = ["gallons","pints","cups","liters","milliliters"]
     
     
-    // dynamic assignment of starting units
-    // dynamic assignment of ending units
+    
+    
+    // runs when selects a conversion from segemented top
+    var typeArray: [String] {
+        var selectedArray : [String] = [""]
+        switch conversionType {
+            case "distance":
+                selectedArray = distance
+            case "time":
+                selectedArray = time
+            case "temp":
+                selectedArray = temp
+            case "volume":
+                selectedArray = volume
+            default:
+                selectedArray = distance
+        }
+       return selectedArray
+    }
+    
+    // runs when amount changes
+    var convertedAmount : Double {
+        var newAmount : Double = 0.0
+        if amount > 0 {
+            // convert
+            switch conversionType {
+                case "distance":
+                    newAmount = convertDistance()
+                case "time":
+                    newAmount = convertTime()
+                default:
+                    newAmount = 0.0
+            }
+            
+            // TODO: deal with the edge case of negative numbers
+            return newAmount
+        }
+        return amount
+    }
+  
+    
+    func convertTime () -> Double {
+        var convertedTime = 0.0
+        // convert to smallet denominator, then multiple
+        switch convertFrom {
+            case "days":
+                convertedTime = amount
+            default:
+                return amount
+        }
+        return convertedTime
+    }
+    
+    // can simplify using divide by and multiply by
+    func convertDistance () ->  Double {
+        // convert to inches, regardless of system
+        
+        var convertedDistance = amount
+        
+        let inchesInFoot : Double = 12
+        let feetInYard : Double = 3
+        let yardsInMile : Double = 1760
+        
+        let mmInCM : Double = 10
+        let cmInMeter : Double = 100
+        let meterInKM : Double = 1000
+        
+        let inchesToMM : Double =  25.4
+ //multiply the inch value by 25.4
+        //Metric to Imperial
+        let mmToInches : Double = 0.0393701 //divide mm value by 25.4
+        
+        //         1 Miles = Meters / 1609.34708789
+        //         .  multiply the number of meters by this .
+        
+        // convert to smallet denominator, inches or mm then multiply
+        switch convertFrom {
+            // convert everything to inches
+            case "miles":
+                // 1 mile is equivalent to 1760yds or 5280ft or 63.360 inches
+                // 12"/foot x 3 ft /yard x 1760 yards
+                // convert to inches
+                convertedDistance *= inchesInFoot * feetInYard * yardsInMile
+            case "yards":
+                convertedDistance *= inchesInFoot * feetInYard
+            case "feet":
+                convertedDistance *= inchesInFoot
+            case "inches":
+                convertedDistance *= 1
+            
+            // converts back to metric : should take inches and conver to mm
+            // multiply by 25.4
+            case "kilometers":
+                convertedDistance *=  meterInKM * cmInMeter * mmInCM * mmToInches
+            case "meters":
+                convertedDistance *= cmInMeter * mmInCM * mmToInches
+            case "centimeters":
+                convertedDistance *= mmInCM * mmToInches
+            case "millimeters":
+                convertedDistance /=  mmToInches
+            default:
+                convertedDistance = amount
+            }
+
+        //Imperial to Metric : CONVERT inches to -----------
+        
+        switch convertTo {
+            case "miles":
+                convertedDistance /= (inchesInFoot * feetInYard * yardsInMile)
+            case "yards":
+                convertedDistance /= (inchesInFoot * feetInYard )
+            case "feet":
+                convertedDistance /= inchesInFoot  //convertedDistance/(inchesInFoot)
+            case "inches":
+                convertedDistance *= 1
+            
+            case "millimeters":
+                convertedDistance *= inchesToMM
+            case "centimeters":
+                convertedDistance *= inchesToMM
+                convertedDistance /= mmInCM
+            case "meters":
+                convertedDistance *= inchesToMM
+                convertedDistance /= (mmInCM * cmInMeter)
+            case "kilometers":
+                convertedDistance *= inchesToMM
+                convertedDistance /= (mmInCM * cmInMeter * meterInKM)
+            
+            default:
+                convertedDistance /= inchesInFoot
+        }
+        return convertedDistance
+    }
+    
+    /*:
+     https://stackoverflow.com/questions/57518852/swiftui-picker-onchange-or-equivalent
+     https://stackoverflow.com/questions/77100045/how-to-dynamically-populate-a-swiftui-picker-segmented-control
+     ForEach(Array(colors.enumerated()), id: \.element) { index, color in
+         Text(color).tag(index)
+     }
+     */
     
     var body: some View {
+       
+        
         NavigationStack{
             // check splitting form
             Form{
@@ -78,18 +219,40 @@ struct ContentView: View {
                         }
                     }
                     .pickerStyle(.segmented)
+                    // .onChange(of: conversionType) { tag in print("Selected: \(tag)") }
                 }
                 .navigationTitle("weConvert")
                 
-                Section(){
+               
+                
+                Section("Select starting unit type:"){
                     // select starting units : based on conversionType variable
-                    // select desired units : based on conversionType variable
-                    // enter amount
+                   
+                    Picker("Convert from:", selection: $convertFrom) {
+                        ForEach(typeArray, id: \.self) {
+                            Text($0)
+                        }
+                    }
                     
+                }
+                Section("Select desired unit type:"){
+                    // select desired units : based on conversionType variable
+                    Picker("Convert to:", selection: $convertTo) {
+                        ForEach(typeArray, id: \.self) {
+                            Text($0)
+                        }
+                    }
+                    //.pickerStyle(WheelPickerStyle())
+                }
+                Section("Enter amount:"){
+                    // enter amount
                     TextField("Amount", value: $amount, format: .number)
                 }
-                Section(){
+                Section("Converted to x"){
                     // Result
+                    Text(convertFrom)
+                    Text(convertTo)
+                    Text(convertedAmount, format: .number)
                 }
             } // /form
         }
