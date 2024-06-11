f734fa0157ed1afa53c4fa7ca0b82c44ef088a16
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-10 20:16:42 -0700

    WeConvert challenge

---
+++ WeConvert/WeConvert/WeConvertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeConvertApp.swift
+//  WeConvert
+//
+//  Created by Jennifer Lee on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WeConvertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
