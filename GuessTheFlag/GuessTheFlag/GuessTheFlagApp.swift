e7d2b122b1b64444c9e237d35c5ad3f59c4957ed
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-12 11:37:12 -0700

    Capture the Flag Project

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Jennifer Lee on 6/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
