42822cb53005c39f225296930443997bc6ea361c
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-15 13:16:49 -0700

    Views and Modifiers

--- ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ViewsAndModifiers/ContentView.swift
@@ -8,16 +8,108 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var useRedText : Bool = false
+    @State private var scale: CGFloat = 1
+    @State private var animationAmount = 1.0
+    
+    let motto1 = Text("Draco dormiens")
+    let motto2 = Text("nunquam titillandus")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Spacer()
+        VStack{
+            motto1
+                .foregroundStyle(.red)
+            motto2
+                .foregroundStyle(.blue)
+            
+           
         }
-        .padding()
+        // using view modifiers
+        // https://developer.apple.com/documentation/swiftui/configuring-views
+        
+        // "environment modifier" -> can apply formatting to all 4 text views simultaneously
+        VStack {
+            Spacer()
+            Text("Gryffindor")
+                .font(.largeTitle)
+            Text("Hufflepuff")
+                .blur(radius: 3)
+            Text("Ravenclaw").animation(.linear(duration: 2))
+            Text("Slytherin")
+        }
+        .font(.title)
+        
+        Button("Tap Me") {
+               animationAmount += 1
+           }
+           .padding(25)
+           .background(.red)
+           .foregroundStyle(.white)
+           .clipShape(.circle)
+           .scaleEffect(animationAmount)
+           .animation(.easeInOut(duration: 2), value: animationAmount)
+        
+//        Button("Tap me") {
+//            scale += 1
+//        }.scaleEffect(scale)
+//        .animation(.default)
+        
+        HStack {
+            //let _ = print("Update SpyView")
+            let _ = Self._printChanges()
+            
+            Button("Hello World") {  // flip the Boolean between true and false
+                useRedText.toggle()
+            }
+                .foregroundStyle(useRedText ? .red : .blue)
+            
+            Button("Hello, world!") { // do nothing
+            }
+            .background(.white)
+            .frame(width: 100, height: 40)
+            
+            Button("Hello, world!") {
+                print(type(of: self.body))
+            }
+            .frame(width: 150, height: 40)
+            .background(.red)
+            
+            
+        } // Vstack
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.orange)
+        
+        
+        Group {
+                Text("Lumos")
+                Text("Obliviate")
+              
+            }
+        VStack{
+            Text("Hello, world!")
+                .padding()
+                .background(.red)
+                .padding()
+                .background(.blue)
+                .padding()
+                .background(.green)
+                .padding()
+                .background(.yellow)
+            
+           
+            
+           
+        }
     }
 }
+
+
+
+
+
+
 
 #Preview {
     ContentView()
--- ViewsAndModifiers/ViewsAndModifiers/UsingModifiers.swift
+++ ViewsAndModifiers/ViewsAndModifiers/UsingModifiers.swift
@@ -10,8 +10,84 @@ import SwiftUI
 struct UsingModifiers: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        
+        Text("Using a Modifier")
+            .modifier(Title()) // using a modifier named modifier
+        
+        
+        Text("MODIFIER extension")
+            .titleStyle() // using a modifier with an extension
+        
+        Section("Using Structs") {
+            CapsuleText(text: "First Capsule")
+                .foregroundStyle(.yellow)
+            CapsuleText(text: "Second Capsule")
+        }
+        
+        
+        Color.blue
+            .frame(width: 300, height: 200)
+            .watermarked(with: "Hacking with Swift")
+        
     }
 }
+
+
+struct Title: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundStyle(.white)
+            .padding()
+            .background(.mint)
+            .clipShape(.rect(cornerRadius: 10))
+    }
+}
+
+extension View {
+    func titleStyle() -> some View {
+        modifier(Title())
+    }
+}
+
+
+struct CapsuleText: View {
+    var text: String
+
+    var body: some View {
+        Text(text)
+            .font(.largeTitle)
+            .padding()
+            .foregroundStyle(.white)
+            .background(.blue)
+            .clipShape(.capsule)
+    }
+}
+
+
+struct Watermark: ViewModifier {
+    var text: String
+
+    func body(content: Content) -> some View {
+        ZStack(alignment: .bottomTrailing) {
+            content
+            Text(text)
+                .font(.caption)
+                .foregroundStyle(.white)
+                .padding(5)
+                .background(.black)
+        }
+    }
+}
+
+extension View {
+    func watermarked(with text: String) -> some View {
+        modifier(Watermark(text: text))
+    }
+}
+
+
 
 #Preview {
     UsingModifiers()
