03df1a2afe55c8ae413c07041b697b1970d74c04
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-16 16:00:09 -0700

    Complete: Rock Paper Scissors.

--- RockPaperScissors/RockPaperScissors/ContentView.swift
+++ RockPaperScissors/RockPaperScissors/ContentView.swift
@@ -14,63 +14,121 @@ struct ContentView: View {
 //    @State private var appChoice : RockPaperScissors = RockPaperScissors.paper
     
     
-    @State private var rps = ["Rock", "Scissors", "Paper" ] //.shuffled()
+    @State private var rps = ["Rock",  "Paper" , "Scissors"] //.shuffled()
+    @State private var winningMoves = ["Paper", "Scissors", "Rock"]
+    
     // Rock > Scissors; Paper > Rock; Scissors > Paper;
     @State private var appChoice : String = ""
-    @State private var userWin : Bool = true
-     
+    @State private var appChoiceInt : Int = 0
+    @State private var userWin : Bool = Bool.random()
+    @State private var winLoseSuccessMessage : String = ""
+    @State private var score : Int = 0
+    @State private var gameCounter : Int = 0
+
     
     var body: some View {
+        Spacer()
+        VStack{
+            Text("Game: \(gameCounter)")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            
+            Text("Score: \(score)")
+                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        }
+        // --------------------------------
+        VStack {
+            Button("Generate App Pick") {
+                play()
+            }.padding(15)
+                .background(.gray)
+                .foregroundStyle(.white)
+                .clipShape(.capsule)
+            
+            HStack {
+                Text("The app picks: ")
+                Text("\(appChoice)")
+                    .font(.largeTitle)
+            }
+        }
+   
+        // --------------------------------
+        HStack{
+            Text("Your goal is to: ")
+            
+            Text(userWin ? "Win " : "Lose")
+                .padding(5)
+                .font(.largeTitle)
+        }
+        // --------------------------------
         VStack {
             Spacer()
-            VStack {
-                
-                
-                HStack{
-                    Button("Rock"){
-                        // evalChoice("Rock")
-                    }
-                        .background(.red)
-                        .foregroundStyle(.white)
-                        .frame(width: 100, height: 40)
-                    Button("Paper"){
-                        // evalChoice("Paper")
-                    }
-                        .background(.blue)
-                        .frame(width: 100, height: 40)
-                        .foregroundStyle(.white)
-                    Button("Scissors"){
-                        // evalChoice("Scissors")
-                    }
-                        .background(.green)
-                        .foregroundStyle(.white)
-                        .frame(width: 100, height: 40)
-                }
-                
-            }
+            Text("\(winLoseSuccessMessage)")
             Spacer()
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-            
-            Text(userWin ? "Your Goal: Win " : "Your Goal: Lose")
-            Text("The app picks: ")
-            Text("\(appChoice)")
-                .font(.largeTitle)
-            Spacer()
-            Spacer()
-            Button("Play") {
-                play()
-            }
-            
         }
         .padding()
+        
+        VStack {
+            
+            Section("Choose your weapon!"){
+            HStack{
+                Button("🪨"){
+                    evalChoice(choice: 2) //"Rock"
+                }
+                    //.padding(25)
+                    //.background(.red)
+                    .foregroundStyle(.white)
+                    //.frame(width: 120, height: 40)
+                    .font(.system(size: 100))
+                    
+                Button("🗒️"){
+                    evalChoice(choice:0) //"Paper"
+                }
+
+                    .font(.system(size: 100))
+                    .foregroundStyle(.white)
+                Button("✂️"){
+                    evalChoice(choice:1) //"Scissors"
+                }
+                    .font(.system(size: 100))
+            }
+            } // group
+            .padding(10)
+            .fontWeight(.bold)
+            
+            Spacer()
+            
+            
+            Spacer()
+            
+        }
     }
     
     func play(){
-        appChoice = rps[Int.random(in: 0...2)]
+        
+        appChoiceInt = Int.random(in: 0...2)
+        appChoice = rps[appChoiceInt]
+        winLoseSuccessMessage = ""
+        userWin = Bool.random()
     }
     
+    func evalChoice(choice: Int){
+        gameCounter += 1
+        // rps rock paper scissors
+        // winningMoves paper scissors rock
+        
+        if winningMoves[choice] == rps[appChoiceInt] {
+            winLoseSuccessMessage = "No Winner. \(winningMoves[choice]) \(choice)  and the app chose \(rps[appChoiceInt])"
+        }  else if (choice == appChoiceInt && userWin) || !userWin && choice != appChoiceInt {
+            // win
+            score += 1
+            winLoseSuccessMessage = "Great Job. 🏆 \(userWin ? winningMoves[choice] : rps[appChoiceInt]) beats \(userWin ? rps[appChoiceInt] : winningMoves[choice])"
+        } else {
+            winLoseSuccessMessage = "You Lose. You chose \(winningMoves[choice]) \(choice)  and the app chose \(rps[appChoiceInt]) \(appChoiceInt). Your goal was to \(userWin ? "win" : "lose")."
+        }
+        
+      
+        return
+    }
     
 }
 
