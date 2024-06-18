a4a7430d9f1116a2d5143320df05ab0133dbc023
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-18 08:04:22 -0700

    Added pickers for Better Rest app

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -14,59 +14,111 @@ import SwiftUI
  */
 struct ContentView: View {
     
-    @State private var sleepAmount = 8.0
     @State private var wakeUp = Date.now
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
     
-    
+    var components = DateComponents()
+       
     var body: some View {
-        VStack {
+        NavigationView {
+            VStack {
+                // why won't this align left
+                HStack{
+                    Text("Today's Date")
+                    Text(Date.now, format: .dateTime.day().month().year())
+                }
+                .frame(alignment: .trailing)
+                
+                
+                Spacer()
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                
+                
+                Text("Daily coffee intake")
+                    .font(.headline)
+                
+                Stepper("\(coffeeAmount) cup(s)", value: $coffeeAmount, in: 1...20)
+                
+            } // Vstack
             
-            //constrains sleep to 4-12 hrs
-//            Stepper("\(sleepAmount) hours", value: $sleepAmount, in: 4...12)
-            Text("Better Rest!")
-                .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-            
-            
-           
-            Text("Wake Up!")
-            
-            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
-            // That will allow all dates in the future, but none in the past – read it as “from the current date up to anything.”
-            
-            
-            
-//            DatePicker("Please enter a date", selection: $wakeUp)
-//                .labelsHidden()
-            
-            
-            /*:
-             https://www.hackingwithswift.com/books/ios-swiftui/selecting-dates-and-times-with-datepicker
-             use displayedComponents to decide what kind of options users should see:
-
-             If you don’t provide this parameter, users see a day, hour, and minute.
-             If you use .date users see month, day, and year.
-             If you use .hourAndMinute users see just the hour and minute components.
-             */
-//            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-            
-            //That still includes the original label so screen readers can use it for VoiceOver, but now they aren’t visible onscreen any more – the date picker won’t be pushed to one side by some empty text.
-            
+            .navigationTitle("BetterRest")
+            .toolbar {
+                Button("Calculate", action: calculateBedtime)
+            }
         }
+
         .padding()
     }
     
     
-    func exampleDates() {
-        // create a second Date instance set to one day in seconds from now
-        let tomorrow = Date.now.addingTimeInterval(86400)
-
-        // create a range from those two
-        let range = Date.now...tomorrow
+    
+    func calculateBedtime() {
     }
+    
+    
+//    func pickingDates(){
+//     //   let components = Calendar.current.dateComponents([.hour, .minute], from: someDate)
+//        let hour = components.hour ?? 0
+//        let minute = components.minute ?? 0
+//        
+//       // components.hour = 8
+//       // components.minute = 0
+//        let date = Calendar.current.date(from: components) ?? .now
+//     
+//    }
+//    func exampleDates() {
+//        // create a second Date instance set to one day in seconds from now
+//        let tomorrow = Date.now.addingTimeInterval(86400)
+//
+//        // create a range from those two
+//        let range = Date.now...tomorrow
+//    }
     
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+//    Text(Date.now.formatted(date: .long, time: .shortened))
+   
+//            Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+//            Text("Wake Up!")
+//
+//            DatePicker("Please enter a date", selection: $wakeUp, in: Date.now...)
+   // That will allow all dates in the future, but none in the past – read it as “from the current date up to anything.”
+   
+   
+   // this is better for accessibility
+//            DatePicker("Please enter a date", selection: $wakeUp)
+//                .labelsHidden()
+   
+   
+   /*:
+    https://www.hackingwithswift.com/books/ios-swiftui/selecting-dates-and-times-with-datepicker
+    use displayedComponents to decide what kind of options users should see:
+
+    If you don’t provide this parameter, users see a day, hour, and minute.
+    If you use .date users see month, day, and year.
+    If you use .hourAndMinute users see just the hour and minute components.
+    */
+//            DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+   
+   //That still includes the original label so screen readers can use it for VoiceOver, but now they aren’t visible onscreen any more – the date picker won’t be pushed to one side by some empty text.
+//constrains sleep to 4-12 hrs
+//            Stepper("\(sleepAmount) hours", value: $sleepAmount, in: 4...12)
+
+
