f0d8f531f2040b912c4f25ecc360aeb01f228b2d
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-17 20:26:30 -0700

    Scaffolding BetterRest Project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jennifer Lee on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BetterRest/BetterRest/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  BetterRest
+//
+//  Created by Jennifer Lee on 6/17/24.
+//
+
+import SwiftUI
+/*:
+ 
+ When do they want to wake up?
+ Roughly how many hours of sleep do they want?
+ How many cups of coffee do they drink per day?
+ */
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
