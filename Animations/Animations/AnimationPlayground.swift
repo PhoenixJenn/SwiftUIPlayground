1046be1ee29cd76524d10cc6fda335d2d9228d3c
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-25 16:15:02 -0700

    Adding button animations

---
+++ Animations/Animations/AnimationPlayground.swift
@@ -0,0 +1,18 @@
+//
+//  AnimationPlayground.swift
+//  Animations
+//
+//  Created by Jennifer Lee on 6/25/24.
+//
+
+import SwiftUI
+
+struct AnimationPlayground: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AnimationPlayground()
+}
