cb94fbfea993a8a176d9ebc3e41334159cc4b648
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-06-25 13:03:10 -0700

    Animations App Base

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Jennifer Lee on 6/25/24.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
