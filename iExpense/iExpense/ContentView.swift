b7060876e2dd85a33551ecc7899931581b75f934
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-01 19:54:52 -0700

    adding observable

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -6,18 +6,29 @@
 // https://www.hackingwithswift.com/books/ios-swiftui/iexpense-introduction
 
 import SwiftUI
+import Observation
 
 struct ContentView: View {
+    @State private var user = User()
+    
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
+            Text("Your name is \(user.firstName) \(user.lastName).")
+
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+                }
         .padding()
     }
 }
+
+@Observable
+class User {
+    var firstName = "Bilbo"
+    var lastName = "Baggins"
+}
+
 
 #Preview {
     ContentView()
