4292f98498dc5945e9b53b6044a3f054b1f4c042
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-02 08:01:07 -0700

    saving objects to user defaults

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -5,6 +5,14 @@
 //  Created by Jennifer Lee on 7/1/24.
 // https://www.hackingwithswift.com/books/ios-swiftui/iexpense-introduction
 
+
+/*:
+ @AppStorage is easier than UserDefaults: it’s one line of code rather than two, and it also means we don’t have to repeat the key name each time. However, right now at least @AppStorage doesn’t make it easy to handle storing complex objects such as Swift structs
+ */
+
+/*:
+ Important: When it comes to you submitting an app to the App Store, Apple asks that you let them know why you're loading and saving data using UserDefaults. This also applies to the @AppStorage property wrapper. It's nothing to worry about, they just want to make sure developers aren't trying to identify users across apps.
+ */
 import SwiftUI
 import Observation
  
@@ -13,12 +21,16 @@ struct ContentView: View {
     @State private var selectedNumber = 0
     var colors = ["Red", "Green", "Blue", "Tartan"]
         @State private var selectedColor = "Red"
+    @AppStorage("tapCount") private var tapCount = 0
     
     var body: some View {
         
         VStack{
             Text("Hello World")
-           
+            Button("Tap count: \(tapCount)") {
+                        tapCount += 1
+            }
+            
                    Picker("Select a number", selection: $selectedNumber) {
                        ForEach(0..<10) {
                            Text("\($0)")
--- iExpense/iExpense/UserDefaults.swift
+++ iExpense/iExpense/UserDefaults.swift
@@ -5,11 +5,26 @@
 //  Created by Jennifer Lee on 7/2/24.
 //
 
+// https://www.hackingwithswift.com/books/ios-swiftui/archiving-swift-objects-with-codable
+// we want to archive a custom type so we can put it into UserDefaults, then unarchive it when it comes back out from UserDefaults.
+
+
 import SwiftUI
 import Foundation
 
+// following examples and used user twice, so renaming
+struct UserObject : Codable {
+    
+    let firstName: String
+    let lastName: String
+}
+
 struct UserDefaults: View {
+    @State private var user = UserObject(firstName: "Taylor", lastName: "Swift")
+    
     @State private var tapCount = Foundation.UserDefaults.standard.integer(forKey: "Tap")
+    
+    
     
     func doNothing(){
         // do nothing
@@ -20,7 +35,6 @@ struct UserDefaults: View {
         VStack{
             Button("Tap count: \(tapCount)") {
                 tapCount += 1
-                
                 Foundation.UserDefaults.standard.set(tapCount, forKey: "Tap")
             }
             
@@ -28,6 +42,21 @@ struct UserDefaults: View {
             Button(action: doNothing) {
                 Label("Add Folder", systemImage: "folder.badge.plus")
             }
+            
+            /*:
+             
+             When we’re coming back the other way – when we have JSON data and we want to convert it to Swift Codable types – we should use JSONDecoder rather than JSONEncoder(), but the process is much the same.
+             */
+            
+            Button("Save User") {
+                let encoder = JSONEncoder()
+
+                if let data = try? encoder.encode(user) {
+                    Foundation.UserDefaults.standard.set(data, forKey: "UserData")
+                }
+            }
+            
+            
         }
         
     }
