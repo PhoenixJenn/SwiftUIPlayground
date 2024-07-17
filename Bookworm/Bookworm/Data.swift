b99584fc268f57a08b6f89646c7195878784f0c8
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-16 20:34:48 -0700

    using SwiftData

---
+++ Bookworm/Bookworm/Data.swift
@@ -0,0 +1,18 @@
+//
+//  Data.swift
+//  Bookworm
+//
+//  Created by Jennifer Lee on 7/16/24.
+//
+
+import SwiftUI
+
+struct Data: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Data()
+}
