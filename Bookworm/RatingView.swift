682d48a46e9a3ff2577b149233639cd47891a179
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-17 16:01:36 -0700

    Added star ratings

---
+++ Bookworm/RatingView.swift
@@ -0,0 +1,18 @@
+//
+//  RatingView.swift
+//  Bookworm
+//
+//  Created by Jennifer Lee on 7/17/24.
+//
+
+import SwiftUI
+
+struct RatingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RatingView()
+}
