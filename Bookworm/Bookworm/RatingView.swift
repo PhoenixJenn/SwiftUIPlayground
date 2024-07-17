04c9cf49e04ef166b7cf108a1a8da1dde1bb54bb
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-17 16:05:00 -0700

    Fixing HStack so entire row doesn't get selected when selecting  a star rating

--- Bookworm/Bookworm/RatingView.swift
+++ Bookworm/Bookworm/RatingView.swift
@@ -39,7 +39,7 @@ struct RatingView: View {
                         .foregroundStyle(number > rating ? offColor : onColor)
                 }
             }
-        }
+        }.buttonStyle(.plain)
     }
     
     func image(for number: Int) -> Image {
