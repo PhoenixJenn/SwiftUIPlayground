e7bd437189df436d979bd7287d61ffa2f1c64636
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-16 20:13:21 -0700

    adding a binding in a view so the parent/calling view can access the value

--- Bookworm/Bookworm/BindingsAndBindables.swift
+++ Bookworm/Bookworm/BindingsAndBindables.swift
@@ -11,7 +11,12 @@ struct BindingsAndBindables: View {
     @State private var rememberMe = false
 
     var body: some View {
-        Toggle("Remember Me", isOn: $rememberMe)
+        VStack {
+            
+                PushButton(title: "Remember Me", isOn: $rememberMe) // added $ so 2way binding
+                //PushButton(title: "Remember Me", isOn: rememberMe)
+                Text(rememberMe ? "On" : "Off")
+                }
     }
     
     
@@ -19,4 +24,25 @@ struct BindingsAndBindables: View {
 
 #Preview {
     BindingsAndBindables()
+}
+
+
+struct PushButton: View {
+    let title: String
+   
+    @Binding var isOn: Bool // now we share 2 way with contentview
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
+        }
+        .padding()
+        .background(LinearGradient(colors: isOn ? onColors : offColors, startPoint: .top, endPoint: .bottom))
+        .foregroundStyle(.white)
+        .clipShape(.capsule)
+        .shadow(radius: isOn ? 0 : 5)
+    }
 }
