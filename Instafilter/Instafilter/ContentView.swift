cfd8c7cd6e3d14dd224497d023811d58cb947c80
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-25 15:40:31 -0700

    Customizing Filters

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -11,6 +11,11 @@
  https://www.hackingwithswift.com/books/ios-swiftui/importing-an-image-into-swiftui-using-photospicker
  https://www.hackingwithswift.com/books/ios-swiftui/basic-image-filtering-using-core-image
  
+ https://www.hackingwithswift.com/books/ios-swiftui/customizing-our-filter-using-confirmationdialog
+ 
+ PREVIEW CRASH
+ https://www.hackingwithswift.com/forums/100-days-of-swift/day-54-instafilter-part-4-applying-filters-error-thread-1-exception-citwirldistortion-0x600002ccea00-setvalue-forundefinedkey-this-class-is-not-key-value-coding-compliant-for-the-key-inputintensity/1783
+ 
  []A NavigationStack so we can show our app’s name at the top.
  []A box prompting users to select a photo, over which we’ll place their imported picture.
  []An “Intensity” slider that will affect how strongly we apply our Core Image filters, stored as a value from 0.0 to 1.0.
@@ -26,7 +31,10 @@ struct ContentView: View {
     @State private var filterIntensity = 0.5
     @State private var selectedItem: PhotosPickerItem?
     
-    @State private var currentFilter = CIFilter.sepiaTone()
+   // @State private var currentFilter = CIFilter.sepiaTone()
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    
+    @State private var showingFilters = false
     
     let context = CIContext()
     
@@ -70,6 +78,7 @@ struct ContentView: View {
                 HStack {
                     Button("Change Filter") {
                         // change filter
+                        changeFilter()
                        
                     }
 
@@ -80,6 +89,17 @@ struct ContentView: View {
             }
             .padding([.horizontal, .bottom])
             .navigationTitle("Instafilter")
+            .confirmationDialog("Select a filter", isPresented: $showingFilters) {
+                // dialog here
+                Button("Crystallize") { setFilter(CIFilter.crystallize()) }
+                Button("Edges") { setFilter(CIFilter.edges()) }
+                Button("Gaussian Blur") { setFilter(CIFilter.gaussianBlur()) }
+                Button("Pixellate") { setFilter(CIFilter.pixellate()) }
+                Button("Sepia Tone") { setFilter(CIFilter.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(CIFilter.unsharpMask()) }
+                Button("Vignette") { setFilter(CIFilter.vignette()) }
+                Button("Cancel", role: .cancel) { }
+            }
         }
         
         
@@ -99,18 +119,34 @@ struct ContentView: View {
     }
     
     func changeFilter() {
+        showingFilters = true
     }
     
     func applyProcessing() {
         
-        currentFilter.intensity = Float(filterIntensity)
+        //currentFilter.intensity = Float(filterIntensity)
+        
+//        currentFilter.setValue( filterIntensity, forKey: kCIInputIntensityKey)
+        
+        let inputKeys = currentFilter.inputKeys
 
+        if inputKeys.contains(kCIInputIntensityKey) { currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey) }
+        
+        
         guard let outputImage = currentFilter.outputImage else { return }
         guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
 
         let uiImage = UIImage(cgImage: cgImage)
         processedImage = Image(uiImage: uiImage)
     }
+    
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+    
     
 }
 
