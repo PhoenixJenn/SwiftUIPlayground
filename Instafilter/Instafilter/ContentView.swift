3f0274e5c83807391f49cf9d1709a0517d3b5a48
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-25 15:22:13 -0700

    Adding images, changing intensity of filter

--- Instafilter/Instafilter/ContentView.swift
+++ Instafilter/Instafilter/ContentView.swift
@@ -5,64 +5,113 @@
 //  Created by Jennifer Lee on 7/24/24.
 //
 
+
+/*:
+ https://www.hackingwithswift.com/books/ios-swiftui/building-our-basic-ui
+ https://www.hackingwithswift.com/books/ios-swiftui/importing-an-image-into-swiftui-using-photospicker
+ https://www.hackingwithswift.com/books/ios-swiftui/basic-image-filtering-using-core-image
+ 
+ []A NavigationStack so we can show our app’s name at the top.
+ []A box prompting users to select a photo, over which we’ll place their imported picture.
+ []An “Intensity” slider that will affect how strongly we apply our Core Image filters, stored as a value from 0.0 to 1.0.
+ []A sharing button to export the processed image from the app.
+ */
 import SwiftUI
 import CoreImage
 import CoreImage.CIFilterBuiltins
+import _PhotosUI_SwiftUI
 
 struct ContentView: View {
-    @State private var image: Image?
-
-        var body: some View {
-           
-            
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    
+    let context = CIContext()
+    
+    var body: some View {
+        
+        NavigationStack {
             VStack {
-                image?
-                    .resizable()
-                    .scaledToFit()
+                Spacer()
+                PhotosPicker(selection: $selectedItem) {
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Import a photo to get started"))
+                    }
+                }
+                .onChange(of: selectedItem, loadImage)
+                
+                
+                // image area
+//                if let processedImage {
+//                    processedImage
+//                        .resizable()
+//                        .scaledToFit()
+//                } else {
+//                    ContentUnavailableView("No Picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+//                }
+                // image area ---
+
+                Spacer()
+
+                HStack {
+                    Text("Intensity")
+            
+                    Slider(value: $filterIntensity)
+                        .onChange(of: filterIntensity, applyProcessing)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                       
+                    }
+
+                    Spacer()
+
+                    // share the picture
+                }
             }
-            .onAppear(perform: loadImage)
+            .padding([.horizontal, .bottom])
+            .navigationTitle("Instafilter")
         }
-
-    func loadImage() {
-        let inputImage = UIImage(resource: .curiousity)
-        let beginImage = CIImage(image: inputImage)
-
-        let context = CIContext()
         
-
-//        let currentFilter = CIFilter.sepiaTone()
-//        currentFilter.inputImage = beginImage
-//        currentFilter.intensity = 1
-
         
-        let currentFilter = CIFilter.twirlDistortion()
-        currentFilter.inputImage = beginImage
-
-        let amount = 1.0
-
-        let inputKeys = currentFilter.inputKeys
-
-        if inputKeys.contains(kCIInputIntensityKey) {
-            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
-        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
-        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
-
-        
-        //get a CIImage from our filter or exit if that fails
-       guard let outputImage = currentFilter.outputImage else { return }
-
-       // attempt to get a CGImage from our CIImage
-       guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
-
-       // convert that to a UIImage
-       let uiImage = UIImage(cgImage: cgImage)
-
-       // and convert that to a SwiftUI image
-       image = Image(uiImage: uiImage)
     }
-//        func loadImage() {
-//            image = Image(.curiousity)
-//        }
+    
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            
+            applyProcessing()
+        }
+    }
+    
+    func changeFilter() {
+    }
+    
+    func applyProcessing() {
+        
+        currentFilter.intensity = Float(filterIntensity)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+    
 }
 
 #Preview {
---
+++ Instafilter/Instafilter/ImageFun.swift
@@ -0,0 +1,70 @@
+//
+//  ImageFun.swift
+//  Instafilter
+//
+//  Created by Jennifer Lee on 7/25/24.
+//
+
+import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
+
+struct ImageFun: View {
+    @State private var image: Image?
+
+        var body: some View {
+           
+            
+            VStack {
+                image?
+                    .resizable()
+                    .scaledToFit()
+            }
+            .onAppear(perform: loadImage)
+        }
+
+    func loadImage() {
+        let inputImage = UIImage(resource: .curiousity)
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+        
+
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+
+        
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+
+        let amount = 1.0
+
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+
+        
+        //get a CIImage from our filter or exit if that fails
+       guard let outputImage = currentFilter.outputImage else { return }
+
+       // attempt to get a CGImage from our CIImage
+       guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+
+       // convert that to a UIImage
+       let uiImage = UIImage(cgImage: cgImage)
+
+       // and convert that to a SwiftUI image
+       image = Image(uiImage: uiImage)
+    }
+//        func loadImage() {
+//            image = Image(.curiousity)
+//        }
+}
+
+#Preview {
+    ImageFun()
+}
