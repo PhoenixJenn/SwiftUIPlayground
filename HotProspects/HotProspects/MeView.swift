59e0f3df8d432c644da9fd426f6957abf631107d
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-27 14:42:33 -0700

    Caching and sharing QR Code

--- HotProspects/HotProspects/MeView.swift
+++ HotProspects/HotProspects/MeView.swift
@@ -6,6 +6,10 @@
 //
 // https://www.hackingwithswift.com/books/ios-swiftui/generating-and-scaling-up-a-qr-code
 
+// https://www.hackingwithswift.com/books/ios-swiftui/adding-a-context-menu-to-an-image
+
+// Long Press QR Code to Share
+
 import SwiftUI
 import CoreImage.CIFilterBuiltins
 
@@ -14,6 +18,7 @@ struct MeView: View {
     @AppStorage("emailAddress") private var emailAddress = "you@yoursite.com"
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
+    @State private var qrCode = UIImage()
     
     var body: some View {
         NavigationStack {
@@ -25,26 +30,34 @@ struct MeView: View {
                 TextField("Email address", text: $emailAddress)
                     .textContentType(.emailAddress)
                     .font(.title)
-                
-                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
+                Image(uiImage: qrCode)
                     .interpolation(.none)
                     .resizable()
                     .scaledToFit()
                     .frame(width: 200, height: 200)
+                    .onAppear(perform: updateCode)
+                    .onChange(of: name, updateCode)
+                    .onChange(of: emailAddress, updateCode)
                     
-                
+                    .contextMenu {
+                        ShareLink(item: Image(uiImage: qrCode), preview: SharePreview("My QR Code", image: Image(uiImage: qrCode)))
+                    }
+                //                Image(uiImage: generateQRCode(from: "\(name)\n\(emailAddress)"))
             }
             .navigationTitle("Your code")
         }
     }
     
-    
+    func updateCode() {
+        qrCode = generateQRCode(from: "\(name)\n\(emailAddress)")
+    }
     func generateQRCode(from string: String) -> UIImage {
         filter.message = Data(string.utf8)
 
+        // cache the QR Code
         if let outputImage = filter.outputImage {
-            if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
-                return UIImage(cgImage: cgImage)
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
             }
         }
 
