3666aa0097812545f355d9c77f4d6cfe46ccf684
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-27 13:19:21 -0700

    Dynamic Filtering / Day 82

--- HotProspects/HotProspects/ProspectsView.swift
+++ HotProspects/HotProspects/ProspectsView.swift
@@ -4,10 +4,17 @@
 //
 //  Created by Jennifer Lee on 7/26/24.
 //
-
+// https://www.hackingwithswift.com/books/ios-swiftui/dynamically-filtering-our-swiftdata-query
+// https://www.hackingwithswift.com/books/ios-swiftui/building-our-tab-bar
 import SwiftUI
+import SwiftData
 
 struct ProspectsView: View {
+    
+    @Query(sort: \Prospect.name) var prospects: [Prospect]
+    @Environment(\.modelContext) var modelContext
+    
+    
     enum FilterType {
         case none, contacted, uncontacted
     }
@@ -27,15 +34,44 @@ struct ProspectsView: View {
         
         
         NavigationStack {
-            Text("Hello, World!")
+            Text("People: \(prospects.count)")
                 .navigationTitle(title)
+                .toolbar {
+                    Button("Scan", systemImage: "qrcode.viewfinder") {
+                        let prospect = Prospect(name: "Paul Hudson", emailAddress: "paul@hackingwithswift.com", isContacted: false)
+                        modelContext.insert(prospect)
+                    }
+                }
+            List(prospects) { prospect in
+                VStack(alignment: .leading) {
+                    Text(prospect.name)
+                        .font(.headline)
+                    Text(prospect.emailAddress)
+                        .foregroundStyle(.secondary)
+                }
+            }
         }
         
         
+    }
+    
+    
+    init(filter: FilterType) {
+        self.filter = filter
+
+        if filter != .none {
+            let showContactedOnly = filter == .contacted
+
+            _prospects = Query(filter: #Predicate {
+                $0.isContacted == showContactedOnly
+            }, sort: [SortDescriptor(\Prospect.name)])
+        }
     }
    
 }
 
 #Preview {
+   
     ProspectsView(filter: .none)
+        .modelContainer(for: Prospect.self)
 }
