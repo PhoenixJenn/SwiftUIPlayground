a455b23e13886fb7537603735a109f1b3f12785d
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-30 14:11:42 -0700

    Adding Challenge text

--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -12,6 +12,18 @@
  Background scenes are not visible to the user, which on iOS means they might be terminated at some point in the future
  
  Command R to build and Run
+ 
+ 
+ Challenge
+ One of the best ways to learn is to write your own code as often as possible, so here are some ways you should try extending this app to make sure you fully understand what’s going on.
+
+ []When adding a card, the text fields keep their current text. Fix that so that the textfields clear themselves after a card is added.
+ []If you drag a card to the right but not far enough to remove it, then release, you see it turn red as it slides back to the center. Why does this happen and how can you fix it? (Tip: think about the way we set offset back to 0 immediately, even though the card hasn’t animated yet. You might solve this with a ternary within a ternary, but a custom modifier will be cleaner.)
+ []For a harder challenge: when the users gets an answer wrong, add that card back into the array so the user can try it again. Doing this successfully means rethinking the ForEach loop, because relying on simple integers isn’t enough – your cards need to be uniquely identifiable.
+ Still thirsty for more? Try upgrading our loading and saving code in two ways:
+
+ []Make it use an alternative approach to saving data, e.g. documents JSON rather than UserDefaults, or SwiftData – this is generally a good idea, so you should get practice with this.
+ []Try to find a way to centralize the loading and saving code for the cards. You might need to experiment a little to find something you like!
  */
 import SwiftUI
 
@@ -163,7 +175,7 @@ struct ContentView: View {
         cards = Array<Card>(repeating: .example, count: 10)
         timeRemaining = 100
         isActive = true
-        loadData()     
+        loadData()
     }
     func loadData() {
         if let data = UserDefaults.standard.data(forKey: "Cards") {
