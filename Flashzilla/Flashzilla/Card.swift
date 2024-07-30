5b8de75398da5fbe2d95271d5ae8893a45b645a0
PhoenixJenn <PhoenixJenn@users.noreply.github.com>
2024-07-30 13:17:53 -0700

    adding card view, updating content view with game logic, adding background image

---
+++ Flashzilla/Flashzilla/Card.swift
@@ -0,0 +1,8 @@
+//
+//  Card.swift
+//  Flashzilla
+//
+//  Created by Jennifer Lee on 7/30/24.
+//
+
+import Foundation
--- Flashzilla/Flashzilla/ContentView.swift
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -16,27 +16,130 @@
 import SwiftUI
 
 struct ContentView: View {
-
-
+    @Environment(\.accessibilityDifferentiateWithoutColor) var accessibilityDifferentiateWithoutColor
     
+    @State private var timeRemaining = 100
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
+     
     @Environment(\.scenePhase) var scenePhase
-
-        var body: some View {
-            Text("Hello, world!")
-                .onChange(of: scenePhase) { oldPhase, newPhase in
-                    if newPhase == .active {
-                        print("Active")
-                    } else if newPhase == .inactive {
-                        print("Inactive")
-                    } else if newPhase == .background {
-                        print("Background")
-                    }
-                }
+    @State private var isActive = true
+    
+    var body: some View {
+        
+        ZStack {
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.75))
+                    .clipShape(.capsule)
+                
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            withAnimation {
+                                removeCard(at: index)
+                                if cards.isEmpty {
+                                    isActive = false
+                                }
+                            }
+                        }
+                        .stacked(at: index, in: cards.count)
+                    }
+                } //zstack
+                .allowsHitTesting(timeRemaining > 0)
+                
+                if cards.isEmpty {
+                    Button("Start Again", action: resetCards)
+                        .padding()
+                        .background(.white)
+                        .foregroundStyle(.black)
+                        .clipShape(.capsule)
+                }
+            } //Vstack
+            
+            if accessibilityDifferentiateWithoutColor {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Image(systemName: "xmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                        Spacer()
+                        Image(systemName: "checkmark.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(.circle)
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            } // ALLY
+            
+            
+        } // zstack
+        .onReceive(timer) { time in
+            guard isActive else { return }
+
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) {
+            if scenePhase == .active {
+                if cards.isEmpty == false {
+                    isActive = true
+                }
+            } else {
+                isActive = false
+            }
+        }
+        
        
+    } // body
+    
+    func removeCard(at index: Int) {
+        cards.remove(at: index)
     }
+    func resetCards() {
+        cards = Array<Card>(repeating: .example, count: 10)
+        timeRemaining = 100
+        isActive = true
+    }
+    
 }
 
 #Preview {
     ContentView()
 }
+
+
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
+
+//            Text("")
+//                .onChange(of: scenePhase) { oldPhase, newPhase in
+//                    if newPhase == .active {
+//                        print("Active")
+//                    } else if newPhase == .inactive {
+//                        print("Inactive")
+//                    } else if newPhase == .background {
+//                        print("Background")
+//                    }
+//                }
